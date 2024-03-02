c9568f90a8c5e41761ec719adcc29bf74ef45bc5
RoqueC <roquecarrizof@gmail.com>
2024-03-02 21:28:35 +0100

    fix

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -4,6 +4,7 @@ $apps = @(
     "Microsoft.PowerShell"
     "JanDeDobbeleer.OhMyPosh"
     "Microsoft.WindowsTerminal"
+    <#
     "7zip.7zip"
     "Git.Git"
     "Microsoft.DotNet.SDK.8"
@@ -17,15 +18,15 @@ $apps = @(
     "OpenJS.NodeJS"
     "VideoLAN.VLC"
     "AgileBits.1Password"
+    #>
 )
 
 foreach ($appName in $apps)
 {
-    Write-Output "Installing $appName"
-    winget install $appName --silent --no-upgrade --accept-package-agreements --accept-source-agreements > setup.log
+    $elapsed = Measure-Command { winget install $appName --silent --no-upgrade --accept-package-agreements --accept-source-agreements > setup.log }
     if ($LASTEXITCODE -eq 0)
     {
-        Write-Output "$appName installed successfully"
+        Write-Output "$appName installed successfully in ${$elapsed.Minutes}m ${$elapsed.Seconds}s ${$elapsed.Milliseconds}ms"
     }
     # 0x8A150061 (APPINSTALLER_CLI_ERROR_PACKAGE_ALREADY_INSTALLED)
     elseif ($LASTEXITCODE -eq -1978335135)
@@ -43,10 +44,10 @@ Write-Output "Updating path variable"
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 Write-Output "Configuring Windows Terminal"
-& $PSScriptRoot + "\windows-terminal\setup.ps1"
+& ($PSScriptRoot + "\windows-terminal\setup.ps1")
 
 Write-Output "Configuring PowerShell"
-& $PSScriptRoot + "\powershell\setup.ps1"
+& ($PSScriptRoot + "\powershell\setup.ps1")
 
 Write-Output "Installing fonts"
-& $PSScriptRoot + "\fonts\setup.ps1"
+& ($PSScriptRoot + "\fonts\setup.ps1")
