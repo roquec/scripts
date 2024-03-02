71208c0a59915f1ee3efaee94b0b60c9931c226a
RoqueC <roquecarrizof@gmail.com>
2024-03-02 23:42:02 +0100

    fix

--- .github/actions/verify-terminal-config.ps1
+++ .github/actions/verify-terminal-config.ps1
@@ -33,8 +33,8 @@ if (-not (Test-Path -Path $profilePath)) {
     Write-Error "PowerShell profile not found."
 }
 
-$referenceProfile = Get-Content -Path "$PSScriptRoot\..\..\windows\powershell\profile.ps1" -Encoding $encoding -Raw | ForEach-Object { $_ -replace "`r`n", "`n" }.Trim()
-$pwshProfile = Get-Content -Path $profilePath -Encoding $encoding -Raw | ForEach-Object { $_ -replace "`r`n", "`n" }.Trim()
+$referenceProfile = Get-Content -Path "$PSScriptRoot\..\..\windows\powershell\profile.ps1" -Encoding $encoding -Raw | ForEach-Object { $_ -replace "`r`n", "`n" }
+$pwshProfile = Get-Content -Path $profilePath -Encoding $encoding -Raw | ForEach-Object { $_ -replace "`r`n", "`n" }
 
 Write-Output $referenceProfile
 Write-Output "------------------------"
