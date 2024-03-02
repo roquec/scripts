ada593b6c6c5d3d001d9a049630a96ce876d55a3
RoqueC <roquecarrizof@gmail.com>
2024-03-02 03:16:36 +0100

    Add windows setup

---
+++ windows/setup.bat
@@ -0,0 +1,16 @@
+mkdir c:\github
+winget install --id=Git.Git -e --accept-package-agreements --accept-source-agreements
+winget install --id=Microsoft.VisualStudioCode -e  
+winget install --id=AgileBits.1Password -e
+winget install --id=7zip.7zip -e 
+winget install --id=Docker.DockerDesktop -e 
+winget install --id=BlastApps.FluentSearch -e  
+winget install --id=Google.Chrome -e  
+winget install --id=LINQPad.LINQPad.7 -e   
+winget install --id=Microsoft.dotnet -e  
+winget install --id=JanDeDobbeleer.OhMyPosh -e  
+winget install --id=Microsoft.PowerAutomateDesktop -e  
+winget install --id=Microsoft.PowerShell -e  
+winget install --id=Microsoft.PowerToys -e  
+winget install --id=VideoLAN.VLC -e  
+winget install --id=WhatsApp.WhatsApp -e
