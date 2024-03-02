4986f7991544da486221838754c4d28751fd5974
RoqueC <roquecarrizof@gmail.com>
2024-03-02 17:02:44 +0100

    fix

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -40,7 +40,9 @@ foreach ($appName in $apps)
     }
 }
 
-Update-PathVariable
+# Update path variable after the installations
+Write-Output "Updating path variable"
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 Write-Output "Configuring Windows Terminal"
 $windowsTerminalSetup = $PSScriptRoot + "\windows-terminal\setup.ps1"
