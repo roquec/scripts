d79613033f5469e39016c7e9f6c32b5c42873b37
RoqueC <roquecarrizof@gmail.com>
2024-03-02 17:12:51 +0100

    fix

--- windows/powershell/setup.ps1
+++ windows/powershell/setup.ps1
@@ -5,7 +5,7 @@ $profilePath = $PROFILE
 # Check if the directory exists
 $profileDir = Split-Path -Path $profilePath
 if (-not (Test-Path -Path $profileDir)) {
-    Write-Error "PowerShell directory not found in [$profileDir]."
+    New-Item -Path $profileDir -ItemType Directory
 }
 
 # Overwrite current profile with reference
