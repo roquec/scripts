58a72c539ca92291530a1ec64c519a3e4f8c95a7
RoqueC <roquecarrizof@gmail.com>
2024-03-03 18:58:23 +0100

    fix

---
+++ windows/terminal-icons/verify.ps1
@@ -0,0 +1,21 @@
+$installed = Get-InstalledModule -Name Terminal-Iconss -erroraction 'silentlycontinue'
+
+$msg += "📁 Terminal Icons Module:"
+if($installed)
+{
+    $ok = $true
+    $msg += "`n    + OK: Terminal-Icons module is installed ✅"
+}
+else
+{
+    $ok = $false
+    $msg += "`n    - Error: Terminal-Icons module is not installed ❌"
+}
+
+$result = New-Object PSObject -Property @{
+    Ok = $ok
+    Msg = $msg
+}
+$result | Add-Member -MemberType ScriptMethod -Name ToString -Force -Value {return "$($this.Msg)"}
+
+$result
--- windows/verify.ps1
+++ windows/verify.ps1
@@ -3,9 +3,10 @@ $fonts = Invoke-Expression -Command "$PSScriptRoot\fonts\verify.ps1"
 $windowsTerminal = Invoke-Expression -Command "$PSScriptRoot\windows-terminal\verify.ps1"
 $powershell = Invoke-Expression -Command "$PSScriptRoot\powershell\verify.ps1"
 $ohMyPosh = Invoke-Expression -Command "$PSScriptRoot\oh-my-posh\verify.ps1"
+$terminalIcons = Invoke-Expression -Command "$PSScriptRoot\terminal-icons\verify.ps1"
 
-$ok = ($winget.Ok -and $fonts.Ok -and $windowsTerminal.Ok -and $powershell.Ok -and $ohMyPosh.Ok)
-$msg = $winget.Msg + "`n`n" + $fonts.Msg + "`n`n" + $windowsTerminal.Msg + "`n`n" + $powershell.Msg + "`n`n" + $ohMyPosh.Msg
+$ok = ($winget.Ok -and $fonts.Ok -and $windowsTerminal.Ok -and $powershell.Ok -and $ohMyPosh.Ok -and $terminalIcons.Ok)
+$msg = $winget.Msg + "`n`n" + $fonts.Msg + "`n`n" + $windowsTerminal.Msg + "`n`n" + $powershell.Msg + "`n`n" + $ohMyPosh.Msg + "`n`n" + $terminalIcons.Msg
 
 $result = New-Object PSObject -Property @{
     Ok = $ok
@@ -14,6 +15,7 @@ $result = New-Object PSObject -Property @{
     WindowsTerminal = $windowsTerminal
     PowerShell = $powershell
     OhMyPosh = $ohMyPosh
+    TerminalIcons = $terminalIcons
     Msg = $msg
 }
 $result | Add-Member -MemberType ScriptMethod -Name ToString -Force -Value {return "$($this.Msg)"}
